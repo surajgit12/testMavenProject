18ecd30e7408c36b7290cf95919a64deaa2c3fe5
suraj <surajgit12@gmail.com>
2026-03-10 11:41:24 +0530

    testmain

--- src/test/java/com/qa/test/LaunchBrowser.java
+++ src/test/java/com/qa/test/LaunchBrowser.java
@@ -14,7 +14,7 @@ public class LaunchBrowser {
 	        // Selenium 4.6+ automatically manages drivers
 	        driver = new ChromeDriver();
 	        driver.manage().window().maximize();
-//test
+//test  // yesy
 	    }
 
 	    @Test(groups ="smoke")
