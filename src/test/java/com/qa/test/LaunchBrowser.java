72b63dbfa9f6d2b5e131664cb9bc902f6f10dcad
suraj <surajgit12@gmail.com>
2026-03-10 16:11:51 +0530

    Test completed

--- src/test/java/com/qa/test/LaunchBrowser.java
+++ src/test/java/com/qa/test/LaunchBrowser.java
@@ -31,6 +31,8 @@ public class LaunchBrowser {
 	        System.out.println("Able tpo launch test page");
 	        System.out.println("Able to test testNg Annotation");
 	        
+	        System.out.println("Param1");
+	        
 	    }
 
 	    @AfterClass
